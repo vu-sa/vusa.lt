aff7b407df253bcedb4cfd6e38af07f68e3e12dd
Justinas Kavoliūnas <justinaskav@gmail.com>
2021-10-18 14:33:48 +0300

    Fixed typo in favicon link

--- resources/views/layouts/user/icons.blade.php
+++ resources/views/layouts/user/icons.blade.php
@@ -8,7 +8,7 @@
 <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/images/icons/favicons/apple-touch-icon-152x152.png" />
 <link rel="icon" type="image/png" href="/images/icons/favicons/favicon-196x196.png" sizes="196x196" />
 <link rel="icon" type="image/png" href="/images/icons/favicons/favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/png" href="/images/icons/favicons/favicons/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="/images/icons/favicons/favicon-32x32.png" sizes="32x32" />
 <link rel="icon" type="image/png" href="/images/icons/favicons/favicon-16x16.png" sizes="16x16" />
 <link rel="icon" type="image/png" href="/images/icons/favicons/favicon-128.png" sizes="128x128" />
 <meta name="msapplication-TileColor" content="#FFFFFF" />
