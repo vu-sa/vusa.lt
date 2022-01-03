002a81371bde3699e1657930e73592e0a9a62ef9
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-01-03 16:01:04 +0200

    Add tawk.to EN

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,7 +40,7 @@ class DatabaseSeeder extends Seeder
         News::factory()->count(25)->create();
         Page::factory()->count(25)->create();
         Saziningai::factory()->count(10)->create();
-        Saziningai_people::factory()->count(10)->create();
+        // Saziningai_people::factory()->count(10)->create();
         Banner::factory()->count(25)->create();
         
     }
--- resources/views/layouts/user/master.blade.php
+++ resources/views/layouts/user/master.blade.php
@@ -70,21 +70,39 @@
         }(document, 'script', 'facebook-jssdk'));
     </script> --}}
 
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/5f71b135f0e7167d00145612/default';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
+    @if (Lang::locale() == 'en')
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API = Tawk_API || {},
+                Tawk_LoadStart = new Date();
+            (function() {
+                var s1 = document.createElement("script"),
+                    s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/5f71b135f0e7167d00145612/1foc6rga3';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
+    @else
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API = Tawk_API || {},
+                Tawk_LoadStart = new Date();
+            (function() {
+                var s1 = document.createElement("script"),
+                    s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/5f71b135f0e7167d00145612/default';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
+    @endif
 
     @include('layouts.user.navbar')
 
