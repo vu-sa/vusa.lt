477d0b9f561bd6eb3af84eea155afc7eac69ee9e
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-06-01 17:34:47 +0300

    Synced padaliniai, lang and non-lang routes

--- routes/web.php
+++ routes/web.php
@@ -83,9 +83,25 @@ Route::group(['prefix' => '{lang?}', 'where' => ['lang' => '(lt|en)']], function
     Route::name('padalinys.')->group(function () {
         Route::domain('{padalinys}.' . $this->host)->group(function () {
             Route::get('/', [Public\MainController::class, 'home'])->name('home');
-            Route::get('{newsString}/{permalink}', [Public\MainController::class, 'news'])->where('newsString', '(naujiena|news)')->name('news');
+            Route::get('naujienos', [Public\MainController::class, 'newsArchive'])->name('newsArchive');
+            Route::get('naujiena/archyvas', [Public\MainController::class, 'newsArchive']);
+            Route::get('saziningai-registracija', [Public\MainController::class, 'saziningaiExamRegistration'])->name('saziningaiExamRegistration');
+            Route::post('saziningai-registracija', [Public\MainController::class, 'storeSaziningaiExamRegistration'])->name('saziningaiExamRegistration.store');
+            Route::get('saziningai-uzregistruoti-egzaminai', [Public\MainController::class, 'saziningaiExams'])->name('saziningaiExams.registered');
+            Route::post('saziningai-uzregistruoti-egzaminai', [Public\MainController::class, 'storeSaziningaiExamObserver'])->name('saziningaiExamObserver.store');
+
+            Route::get('ataskaita-2022', [Public\MainController::class, 'ataskaita2022']);
+            Route::get('ataskaita-2022/{permalink}', [Public\MainController::class, 'ataskaita2022'])->where('permalink', '.*')->name('ataskaita2022');
+
             Route::get('kontaktai', [Public\MainController::class, 'contactsCategory'])->name('contacts');
+            Route::get('kontaktai/paieska', [Public\MainController::class, 'searchContacts'])->name('contacts.search');
             Route::get('kontaktai/{alias}', [Public\MainController::class, 'contacts'])->name('contacts.alias');
+
+            Route::get('{newsString}/{permalink}', [Public\MainController::class, 'news'])->where('news_string', '(naujiena|news)')->name('news');
+
+            Route::post('search', [Public\MainController::class, 'search'])->name('search');
+
+            Route::get('mainNews', [Public\MainController::class, 'getMainNews']);
             Route::get('{permalink}', [Public\MainController::class, 'page'])->where('permalink', '.*')->name('page');
         });
     });
@@ -96,13 +112,21 @@ Route::group(['prefix' => '{lang?}', 'where' => ['lang' => '(lt|en)']], function
             Route::get('naujienos', [Public\MainController::class, 'newsArchive'])->name('newsArchive');
             Route::get('naujiena/archyvas', [Public\MainController::class, 'newsArchive']);
             Route::get('saziningai-registracija', [Public\MainController::class, 'saziningaiExamRegistration'])->name('saziningaiExamRegistration');
+            Route::post('saziningai-registracija', [Public\MainController::class, 'storeSaziningaiExamRegistration'])->name('saziningaiExamRegistration.store');
             Route::get('saziningai-uzregistruoti-egzaminai', [Public\MainController::class, 'saziningaiExams'])->name('saziningaiExams.registered');
+            Route::post('saziningai-uzregistruoti-egzaminai', [Public\MainController::class, 'storeSaziningaiExamObserver'])->name('saziningaiExamObserver.store');
+
             Route::get('ataskaita-2022', [Public\MainController::class, 'ataskaita2022']);
             Route::get('ataskaita-2022/{permalink}', [Public\MainController::class, 'ataskaita2022'])->where('permalink', '.*')->name('ataskaita2022');
+
             Route::get('kontaktai', [Public\MainController::class, 'contactsCategory'])->name('contacts');
             Route::get('kontaktai/paieska', [Public\MainController::class, 'searchContacts'])->name('contacts.search');
             Route::get('kontaktai/{alias}', [Public\MainController::class, 'contacts'])->name('contacts.alias');
+
             Route::get('{newsString}/{permalink}', [Public\MainController::class, 'news'])->where('news_string', '(naujiena|news)')->name('news');
+
+            Route::post('search', [Public\MainController::class, 'search'])->name('search');
+
             Route::get('mainNews', [Public\MainController::class, 'getMainNews']);
             Route::get('{permalink}', [Public\MainController::class, 'page'])->where('permalink', '.*')->name('page');
         });
@@ -113,10 +137,12 @@ Route::get('/', [Public\MainController::class, 'home'])->name('home');
 Route::get('naujienos', [Public\MainController::class, 'newsArchive'])->name('newsArchive');
 // redirect /naujiena/archyvas to newsArchive
 Route::get('naujiena/archyvas', [Public\MainController::class, 'newsArchive']);
+
 Route::get('saziningai-registracija', [Public\MainController::class, 'saziningaiExamRegistration'])->name('saziningaiExamRegistration');
 Route::post('saziningai-registracija', [Public\MainController::class, 'storeSaziningaiExamRegistration'])->name('saziningaiExamRegistration.store');
 Route::get('saziningai-uzregistruoti-egzaminai', [Public\MainController::class, 'saziningaiExams'])->name('saziningaiExams.registered');
 Route::post('saziningai-uzregistruoti-egzaminai', [Public\MainController::class, 'storeSaziningaiExamObserver'])->name('saziningaiExamObserver.store');
+
 Route::get('ataskaita-2022', [Public\MainController::class, 'ataskaita2022']);
 Route::get('ataskaita-2022/{permalink}', [Public\MainController::class, 'ataskaita2022'])->where('permalink', '.*')->name('ataskaita2022');
 Route::get('{newsString}/{permalink}', [Public\MainController::class, 'news'])->where('newsString', '(naujiena|news)')->name('news');
@@ -127,6 +153,7 @@ Route::get('kontaktai/{alias}', [Public\MainController::class, 'contacts'])->nam
 
 Route::post('search', [Public\MainController::class, 'search'])->name('search');
 
+Route::get('mainNews', [Public\MainController::class, 'getMainNews']);
 Route::get('{permalink}', [Public\MainController::class, 'page'])->where('permalink', '.*')->name('page');
 
 /**
