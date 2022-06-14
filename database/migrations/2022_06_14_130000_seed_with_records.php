e572ff4f688a7afe9c63688e2050269fb2c8eaf7
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-06-14 14:11:18 +0300

    Migration and documentation update (#83)
    * Some .env and readme.md changes
    
    * Small readme update
    
    * Updated readme
    
    * Cleaned up migrations
    
    * Updated .gitignore

---
+++ database/migrations/2022_06_14_130000_seed_with_records.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class SeedWithRecords extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (config('app.env') == 'local') {
+            DB::unprepared(file_get_contents('database/vusa_www-2022.sql'));
+        }
+    }
+}
