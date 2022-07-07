b1a5b2122b822282298aef34466242f7954a4d85
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-07-07 13:19:54 +0300

    Add vite config

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import i18n from "laravel-vue-i18n/vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [
+    laravel(["resources/js/app.js"]),
+    // react(),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+    i18n(),
+  ],
+  resolve: {
+    alias: {
+      "@": "/resources/js",
+    },
+  },
+});
