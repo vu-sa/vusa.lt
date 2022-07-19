3a6ba141495c070b764107e3d32fdbcd6fd923fd
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-07-19 22:00:01 +0300

    Remove storybook, other typescript options

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,26 @@
+// TODO: reinstall Storybook when v7 is released. v6 has React deps which messes with IDEs. And it also has React and webpack deps
+
 module.exports = {
   stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: [
-    "@storybook/addon-essentials",
-    // "@storybook/addon-links",
-  ],
+  addons: ["@storybook/addon-essentials"],
   framework: "@storybook/vue3",
   core: {
     builder: "@storybook/builder-vite",
   },
   features: {
     storyStoreV7: true,
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
   },
 };
