f3f580ef97d5fd6b07c66432bc24838f34ccbd13
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-09-25 00:04:14 +0300

    Add overflow to Tiptap buttons

--- resources/js/Components/TipTap.vue
+++ resources/js/Components/TipTap.vue
@@ -4,7 +4,7 @@
   >
     <div
       v-if="editor"
-      class="tiptap-navbar flex items-center rounded-t-lg bg-gradient-to-tr from-zinc-200 to-zinc-100 p-2 shadow-sm dark:from-zinc-800/90 dark:to-zinc-700/90"
+      class="tiptap-navbar flex items-center overflow-auto rounded-t-lg bg-gradient-to-tr from-zinc-200 to-zinc-100 p-2 shadow-sm dark:from-zinc-800/90 dark:to-zinc-700/90"
     >
       <!-- <strong class="mb-4">Funkcijos</strong> -->
       <!-- <br /> -->
