e8e672359f6457a0e38a2f37e472b309fc6a327c
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-09-25 18:41:06 +0300

    Make layout more responsive

--- resources/js/Components/Public/ConsentCard.vue
+++ resources/js/Components/Public/ConsentCard.vue
@@ -1,30 +1,31 @@
 <template>
   <div class="fixed bottom-0 z-50 w-screen">
     <div
-      class="mx-auto mb-12 flex h-fit w-fit items-center gap-4 rounded-md bg-white px-4 py-3 shadow-md dark:bg-zinc-700"
+      class="mx-auto mb-12 flex h-fit w-fit max-w-sm flex-wrap items-center justify-center gap-4 bg-white p-4 shadow-md dark:bg-zinc-700 sm:rounded-md md:max-w-none"
     >
       <p
         class="prose-sm prose flex h-fit items-center gap-4 px-4 dark:prose-invert"
       >
-        <NIcon :component="Cookies24Regular"></NIcon>
+        <NIcon :size="20" :component="Cookies24Regular"></NIcon>
 
         <span>
-          Naudojame tik seanso slapukus, kurie yra privalomi tinklalapio
-          veikimui.
+          Naudojame seanso slapukus, kurie yra privalomi tinklalapio veikimui.
         </span>
       </p>
-      <NButton size="small" round secondary :tag="Link" href="/privatumas"
-        >Privatumo politika</NButton
-      >
+      <div class="flex gap-2">
+        <NButton size="small" round secondary :tag="Link" href="/privatumas"
+          >Privatumo politika</NButton
+        >
 
-      <NButton
-        size="small"
-        round
-        secondary
-        type="warning"
-        @click="$emit('okayCookieConsent')"
-        >Gerai</NButton
-      >
+        <NButton
+          size="small"
+          round
+          secondary
+          type="warning"
+          @click="$emit('okayCookieConsent')"
+          >Gerai</NButton
+        >
+      </div>
     </div>
   </div>
 </template>
