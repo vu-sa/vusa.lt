d782a2b721eb7d476d5a94fb7b32b7e5990043af
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-09-29 01:23:07 +0300

    Add Sharepoint file view and upload logic (#117)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,9 @@
     <meta name="color-scheme" content="light dark">
     {{-- <meta name="theme-color" content="#bd2835" /> --}}
 
+    {{-- CSRF --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     {{-- Atom Feed --}}
     @include('feed::links')
 
