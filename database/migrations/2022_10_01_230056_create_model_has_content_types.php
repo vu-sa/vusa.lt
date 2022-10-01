965f82821cd1461018e1106ec82604a111a86d5f
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-10-01 23:05:22 +0300

    Redo content type table

--- database/migrations/2022_10_01_230056_create_model_has_content_types.php
+++ database/migrations/2022_10_01_230056_create_model_has_content_types.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,14 +12,10 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {        
-        Schema::create('content_types', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('parent_id')->nullable()->constrained('types')->nullOnDelete();
-            $table->string('title')->nullable();
-            $table->string('description')->nullable();
-            $table->string('slug')->nullable();
-            $table->json('extra_attributes')->nullable();
+    {
+        Schema::create('model_has_content_types', function (Blueprint $table) {
+            $table->foreignId('content_type_id')->constrained('content_types')->cascadeOnDelete();
+            $table->morphs('model');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('content_types');
+        Schema::dropIfExists('model_has_content_types');
     }
 };
