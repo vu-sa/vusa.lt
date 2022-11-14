38e91d531eecd13107ad82c91fbf736d3fe38c89
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-11-14 04:23:28 +0200

    Adjust slide style

--- resources/js/Components/Public/FullWidth/BannerCarousel.vue
+++ resources/js/Components/Public/FullWidth/BannerCarousel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-8 mb-4 rounded-lg p-4 lg:mx-16 lg:mb-8 lg:px-8">
-    <h2 class="mb-2">
+    <h2 class="mb-4">
       Dėkojame mūsų studentų festivalio
       <a target="_blank" href="https://www.facebook.com/events/1166674644261425"
         >Studentify</a
@@ -16,9 +16,12 @@
       :interval="2000"
       :show-dots="false"
     >
-      <NCarouselItem v-for="banner in banners" :key="banner.id">
+      <NCarouselItem class="my-auto" v-for="banner in banners" :key="banner.id">
         <a target="_blank" :href="banner.link_url">
-          <img class="rounded-sm shadow-md" :src="banner.image_url" />
+          <img
+            class="rounded-sm shadow-md w-3/4 lg:max-h-32 object-contain"
+            :src="banner.image_url"
+          />
         </a>
       </NCarouselItem>
     </NCarousel>
