1047cb889c128b3f0f6b339f2525a7ed718fa795
Justinas Kavoliūnas <justinaskav@gmail.com>
2022-12-26 15:32:14 +0200

    Componentize question card

---
+++ resources/js/Components/Cards/QuestionCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <NCard
+    size="small"
+    segmented
+    class="bg-red my-2 cursor-pointer shadow-sm"
+    style="border-radius: 0.75em"
+    hoverable
+    as="button"
+    @click="Inertia.visit(route('questions.show', question.id))"
+    ><template #header>{{ question.title }}</template>
+    <template #footer>
+      <div class="flex items-center justify-between gap-2">
+        <StatusTag :status="question.status"></StatusTag>
+        <div class="inline-flex items-center gap-1">
+          <NIcon :component="Sparkle20Filled" /><span>{{
+            question.doings_count
+          }}</span>
+        </div>
+      </div>
+    </template>
+    <div class="flex items-center gap-1">
+      <NIcon :component="CalendarClock24Filled" />
+      <time>{{ getYYYYMMMM(question.created_at * 1000) }}</time>
+    </div>
+  </NCard>
+</template>
+
+<script setup lang="tsx">
+import { CalendarClock24Filled, Sparkle20Filled } from "@vicons/fluent";
+import { Inertia } from "@inertiajs/inertia";
+import { NCard, NIcon } from "naive-ui";
+import route from "ziggy-js";
+
+import { getYYYYMMMM } from "@/Composables/getRelativeTime";
+import StatusTag from "@/Components/Tags/StatusTag.vue";
+
+defineProps<{
+  question: App.Models.Question;
+}>();
+</script>
--- resources/js/Components/TabPaneContent/QuestionsTabPane.vue
+++ resources/js/Components/TabPaneContent/QuestionsTabPane.vue
@@ -1,31 +1,10 @@
 <template>
   <div class="grid grid-cols-4 gap-x-4">
-    <NCard
+    <QuestionCard
       v-for="question in questions"
       :key="question.id"
-      size="small"
-      segmented
-      class="bg-red my-2 cursor-pointer shadow-sm"
-      style="border-radius: 0.75em"
-      hoverable
-      as="button"
-      @click="Inertia.visit(route('questions.show', question.id))"
-      ><template #header>{{ question.title }}</template>
-      <template #footer>
-        <div class="flex items-center justify-between gap-2">
-          <StatusTag :status="question.status"></StatusTag>
-          <div class="inline-flex items-center gap-1">
-            <NIcon :component="Sparkle20Filled" /><span>{{
-              question.doings_count
-            }}</span>
-          </div>
-        </div>
-      </template>
-      <div class="flex items-center gap-1">
-        <NIcon :component="CalendarClock24Filled" />
-        <time>{{ getYYYYMMMM(question.created_at * 1000) }}</time>
-      </div>
-    </NCard>
+      :question="question"
+    ></QuestionCard>
     <div
       v-if="institution"
       role="button"
@@ -56,19 +35,12 @@
 </template>
 
 <script setup lang="tsx">
-import {
-  BookQuestionMark20Filled,
-  CalendarClock24Filled,
-  Sparkle20Filled,
-} from "@vicons/fluent";
-import { Inertia } from "@inertiajs/inertia";
-import { NCard, NIcon, NModal } from "naive-ui";
+import { BookQuestionMark20Filled } from "@vicons/fluent";
+import { NIcon, NModal } from "naive-ui";
 import { ref } from "vue";
-import route from "ziggy-js";
 
-import { getYYYYMMMM } from "@/Composables/getRelativeTime";
+import QuestionCard from "@/Components/Cards/QuestionCard.vue";
 import QuestionForm from "@/Components/AdminForms/QuestionForm.vue";
-import StatusTag from "@/Components/Tags/StatusTag.vue";
 
 defineProps<{
   questions: App.Models.Question[];
