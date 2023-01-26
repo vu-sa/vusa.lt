e7002e1d35659eb5012a6a8c9d0ffaad1ca3acad
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-01-26 17:06:40 +0200

    Add email to popover

--- resources/js/Components/Avatars/UserPopover.vue
+++ resources/js/Components/Avatars/UserPopover.vue
@@ -3,7 +3,7 @@
     class="not-prose rounded-sm"
     :show-arrow="false"
     raw
-    style="max-width: 150px"
+    style="max-width: 200px"
   >
     <template #trigger>
       <UserAvatar v-if="!showName" :user="user" :size="size"></UserAvatar>
@@ -17,13 +17,27 @@
       class="flex flex-col gap-2 rounded-sm bg-white p-2 text-xs text-zinc-700 dark:bg-zinc-700 dark:text-zinc-200"
     >
       <span class="font-bold dark:text-zinc-100">{{ user.name }}</span>
-      <a v-if="user.phone" :href="`tel:${user.phone}`">{{ user.phone }}</a>
+      <div v-if="user.email" class="flex items-center gap-2">
+        <NIcon :size="12" :component="Mail24Regular" /><a
+          class="line-clamp-1"
+          :href="`mailto:${user.email}`"
+          >{{ user.email }}</a
+        >
+      </div>
+      <div v-if="user.phone" class="flex items-center gap-2">
+        <NIcon :size="12" :component="Phone24Regular" /><a
+          class="line-clamp-1"
+          :href="`tel:${user.phone}`"
+          >{{ user.phone }}</a
+        >
+      </div>
     </div>
   </NPopover>
 </template>
 
 <script setup lang="ts">
-import { NPopover } from "naive-ui";
+import { Mail24Regular, Phone24Regular } from "@vicons/fluent";
+import { NIcon, NPopover } from "naive-ui";
 import { computed } from "vue";
 import UserAvatar from "./UserAvatar.vue";
 
