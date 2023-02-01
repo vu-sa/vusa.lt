4eff819ced005e2105442358054bbdbb7d4f6958
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-02-01 20:27:49 +0200

    Fix test

--- tests/Feature/Notifications/CommentNotificationTest.php
+++ tests/Feature/Notifications/CommentNotificationTest.php
@@ -3,14 +3,17 @@
 use App\Events\CommentPosted;
 use App\Models\Comment;
 use App\Models\Doing;
-use App\Notifications\CommentNotification;
 use App\Models\User;
+use App\Notifications\ModelCommented;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Notification;
 
 it('asserts that a comment notification is sent when an user comments on a doing', function () {
 
-    $user = User::factory()->create();
-    $doing = Doing::factory()->create();
+    $doing = Doing::factory()->has(User::factory()->count(1))->create();
+
+    // get user from doing
+    $user = $doing->users->first();
 
     $comment = Comment::factory()->for($user)->for($doing, 'commentable')->create();
 
@@ -19,16 +22,8 @@ it('asserts that a comment notification is sent when an user comments on a doing
     CommentPosted::dispatch($comment);
 
     Notification::assertSentTo(
-        $user,
-        CommentNotification::class,
-        function ($notification, $channels) use ($comment) {
-            return $notification->comment->id === $comment->id;
-        }
+        [$user],
+        ModelCommented::class
     );
-
-    // remove created models after test
-    // $comment->delete();
-    // $doing->delete();
-    // $user->delete();
 });
 
