c3835357adad112c14cea166f2f641d09b70428d
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-02-02 13:25:21 +0200

    Add notifications directly to notification bell

--- resources/js/Features/Admin/Notifications/NotificationBell.vue
+++ resources/js/Features/Admin/Notifications/NotificationBell.vue
@@ -49,7 +49,19 @@
 </template>
 
 <script setup lang="tsx">
-import { NBadge, NButton, NIcon, NPopover, useMessage } from "naive-ui";
+import {
+  NAvatar,
+  NBadge,
+  NButton,
+  NIcon,
+  NPopover,
+  useMessage,
+  useNotification,
+} from "naive-ui";
+import {
+  type UseWebNotificationOptions,
+  useWebNotification,
+} from "@vueuse/core";
 import { ref } from "vue";
 import { usePage } from "@inertiajs/vue3";
 import Icons from "@/Types/Icons/regular";
@@ -57,7 +69,7 @@ import Icons from "@/Types/Icons/regular";
 import { CheckmarkCircle24Regular } from "@vicons/fluent";
 import { useAxios } from "@vueuse/integrations/useAxios";
 import NotificationItem from "./NotificationItem.vue";
-// import UseWebNotifications from "./UseWebNotifications.vue";
+import type { NotificationData } from "./NotificationItem.vue";
 
 const notifications = ref(usePage().props.auth?.user?.unreadNotifications);
 
@@ -87,4 +99,44 @@ const handleAllRead = async () => {
     message.success("Visi pranešimai pažymėti kaip perskaityti.");
   }
 };
+
+const notification = useNotification();
+
+window.Echo.private(
+  "App.Models.User." + usePage().props.auth?.user.id
+).notification((notificationSent: NotificationData) => {
+  notification.info({
+    content() {
+      return <div v-html={notificationSent.text}></div>;
+    },
+    avatar() {
+      return (
+        <NAvatar
+          src={
+            notificationSent.subject?.image ??
+            usePage().props.auth?.user.profile_photo_path
+          }
+        ></NAvatar>
+      );
+    },
+  });
+
+  const options: UseWebNotificationOptions = {
+    title: notificationSent.text.replaceAll(/<\/?[^>]+(>|$)/gi, ""),
+    dir: "auto",
+    lang: usePage().props.locale,
+    renotify: true,
+    tag: "notification",
+    icon: notificationSent.subject?.image ?? usePage().props.auth?.user,
+  };
+
+  const { isSupported, onClick, show } = useWebNotification(options);
+
+  if (isSupported.value) {
+    show();
+    onClick.on((evt: Event) => {
+      window.location.assign(notificationSent.object.url);
+    });
+  }
+});
 </script>
