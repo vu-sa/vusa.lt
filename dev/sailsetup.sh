c7f433a4eb4a73ac6acad041ff24ace18173cb5a
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-03-21 16:01:21 +0200

    Update setup script

--- dev/sailsetup.sh
+++ dev/sailsetup.sh
@@ -9,13 +9,18 @@ docker run --rm \
     -v $(pwd):/var/www/html \
     -w /var/www/html \
     laravelsail/php81-composer:latest \
-    composer install --ignore-platform-reqs &&
+    composer install --ignore-platform-reqs
 
 # alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail' &&
 ./vendor/bin/sail up -d &&
 ./vendor/bin/sail composer install &&
 ./vendor/bin/sail artisan key:generate &&
-./vendor/bin/sail npm install &&
-./vendor/bin/sail artisan storage:link &&
+./vendor/bin/sail npm install
+
+# check if public/storage is linked
+if [ ! -L public/storage ]; then
+    ./vendor/bin/sail artisan storage:link
+fi
+
 ./vendor/bin/sail artisan migrate:fresh --seed &&
 ./vendor/bin/sail npm run dev
