b372e517cafacb5e51ce729598ceb7bb6a0e86a7
justinaskav <justinaskav@users.noreply.github.com>
2023-03-21 14:28:33 +0000

    PHP Linting (Pint)

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use App\Models\Role;
+use App\Models\User;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
