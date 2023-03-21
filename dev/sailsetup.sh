2ca17fec905dfebd9ff579eaac03351d22cf1607
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-03-21 13:49:23 +0200

    Update dev env prep

--- dev/sailsetup.sh
+++ dev/sailsetup.sh
@@ -2,6 +2,8 @@
 
 # Run only for the first time to setup the sail environment
 
+cp .env.example .env &&
+
 docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v $(pwd):/var/www/html \
@@ -9,13 +11,11 @@ docker run --rm \
     laravelsail/php81-composer:latest \
     composer install --ignore-platform-reqs &&
 
-cp .env.example .env &&
-
 # alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail' &&
 ./vendor/bin/sail up -d &&
 ./vendor/bin/sail composer install &&
 ./vendor/bin/sail artisan key:generate &&
 ./vendor/bin/sail npm install &&
 ./vendor/bin/sail artisan storage:link &&
-./vendor/bin/sail artisan migrate:fresh --seed && 
+./vendor/bin/sail artisan migrate:fresh --seed &&
 ./vendor/bin/sail npm run dev
