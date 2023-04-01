8409ee586d15df64749b0814b262d4abdc083ef4
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-04-01 14:48:15 +0300

    Add model permission creation action

---
+++ app/Actions/CreateModelPermissions.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Actions;
+
+use App\Enums\CRUDEnum;
+use App\Enums\PermissionScopeEnum;
+use App\Models\Permission;
+use Illuminate\Support\Str;
+
+class CreateModelPermissions {
+
+    public static function execute(array $modelLabels) {
+
+        // foreach (ModelEnum::toLabels() as $model) {
+        foreach ($modelLabels as $model) {
+            $pluralizedModel = Str::plural($model);
+
+            foreach (CRUDEnum::toLabels() as $crud) {
+                foreach (PermissionScopeEnum::toLabels() as $scope) {
+                    $permissionsToCreate[] = $pluralizedModel.'.'.$crud.'.'.$scope;
+                }
+            }
+        }
+
+        foreach ($permissionsToCreate as $permission) {
+            Permission::create([
+                'name' => $permission,
+                'guard_name' => 'web',
+            ]);
+        }
+    }
+
+}
