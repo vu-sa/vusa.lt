322d727158d12426cdbd4840fe68570df8e97e6a
justinaskav <justinaskav@users.noreply.github.com>
2023-07-27 16:17:32 +0000

    PHP Linting (Pint)

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Models\Pivots\ReservationResource;
 use App\Models\Traits\HasComments;
 use App\Models\Traits\HasTasks;
-use App\Models\Traits\HasTranslations;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
