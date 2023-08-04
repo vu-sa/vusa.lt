3656f0078953ca96991284747292500a48cd99bb
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-08-04 12:44:06 +0300

    Make the uploads rewrite middleware more clear

--- app/Http/Middleware/RewriteUploadsUrl.php
+++ app/Http/Middleware/RewriteUploadsUrl.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class UrlRewrite
+class RewriteUploadsUrl
 {
     /**
      * Handle an incoming request.
@@ -14,23 +14,18 @@ class UrlRewrite
      */
     public function handle($request, Closure $next)
     {
+        /**
+         * Historically, many links in the database were stored without the
+         * .../files/... part. This middleware redirects those links to the
+         * correct location.
+         */
+
         if (substr(request()->path(), 0, 8) == 'uploads/' &&
         substr(request()->path(), 0, 13) != 'uploads/files') {
             $upload_path = '/uploads/files'.substr(request()->getPathInfo(), 8);
 
             return redirect($upload_path)->send();
         }
-
-        // Visos nuorodos perrašomos į '/lt'
-
-        // if (
-        //     substr(request()->path(), 0, 3) != "lt/" &&
-        //     request()->path() != 'lt' &&
-        //     request()->path() != "en" &&
-        //     substr(request()->path(), 0, 3) != "en/"
-        // ) {
-        //     return redirect('/lt' . request()->getPathInfo())->send();
-        // }
 
         return $next($request);
     }
