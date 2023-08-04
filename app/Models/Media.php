aeb762af77293daddbf46dac91afa7fe2f0710bd
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-08-04 13:08:40 +0300

    Readd Media model (needed for typescript transform)

---
+++ app/Models/Media.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\MediaCollections\Models\Media as ModelsMedia;
+
+// This is needed so typescript transformer gets this model
+class Media extends ModelsMedia {}
