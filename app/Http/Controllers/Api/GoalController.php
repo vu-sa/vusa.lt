abbef0d9df8328da6448b053401f64c3e3e52ad4
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-08-09 01:36:04 +0300

    Remove unused goal api routes

--- app/Http/Controllers/Api/GoalController.php
+++ app/Http/Controllers/Api/GoalController.php
@@ -13,53 +13,10 @@ class GoalController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $goals = Goal::all();
+    // public function index()
+    // {
+    //     $goals = Goal::all();
 
-        return response()->json($goals);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    //     return response()->json($goals);
+    // }
 }
