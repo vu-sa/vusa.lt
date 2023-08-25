b9f7a897ee1792fa84751b8e50f45fa6fdef0a36
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-08-26 00:37:10 +0300

    Add ulids to dutiables

---
+++ database/migrations/2023_08_25_233800_add_id_to_dutiables.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Pivots\Dutiable;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('dutiables', function (Blueprint $table) {
+            // add ulid and make it first column
+            $table->ulid('id')->first();
+        });
+
+        // add ulid to every existing dutiable
+        DB::table('dutiables')->get()->each(function ($dutiable) {
+            $ulid = strtolower(\Illuminate\Support\Str::ulid());
+            DB::table('dutiables')->where('duty_id', $dutiable->duty_id)->where('dutiable_id', $dutiable->dutiable_id)->where('start_date', $dutiable->start_date)->update(['id' => $ulid]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('dutiables', function (Blueprint $table) {
+            $table->dropColumn('id');
+        });
+    }
+};
