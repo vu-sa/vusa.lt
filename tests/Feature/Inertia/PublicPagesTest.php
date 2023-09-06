5773ca742b9e8e422536f7b9581ba993bf3969b7
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-09-06 23:34:32 +0300

    Add public page testing

--- tests/Feature/Inertia/PublicPagesTest.php
+++ tests/Feature/Inertia/PublicPagesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Inertia;
 
+use App\Models\Institution;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Inertia\Testing\AssertableInertia as Assert;
 use PHPUnit\Framework\Attributes\CoversNothing;
@@ -68,18 +69,49 @@ class PublicPagesTest extends TestCase
                 ->component('Public/NewsArchive')
                 ->has('news', fn (Assert $page) => $page
                     ->has('data')
+                    ->etc()
                 )
             );
     }
 
     public function test_can_open_news(): void
     {
-        $news = \App\Models\News::factory()->create();
+        $news = \App\Models\News::query()->inRandomOrder()->first();
 
         $this->get(route('news', ['subdomain' => 'www', 'lang' => 'lt', 'news' => $news->permalink, 'newsString' => 'naujiena']))
             ->assertInertia(fn (Assert $page) => $page
-                ->component('Public/News')
-                ->has('news')
+                ->component('Public/NewsPage')
+                ->has('article')
+            );
+    }
+
+    public function test_can_open_student_representative_page(): void
+    {
+        $this->get(route('contacts.studentRepresentatives', ['subdomain' => 'www', 'lang' => 'lt']))
+            ->assertInertia(fn (Assert $page) => $page
+                ->component('Public/Contacts/ShowStudentReps')
+                ->has('types')
+            );
+    }
+
+    public function test_can_open_institution_page(): void
+    {
+        $institution = Institution::query()->inRandomOrder()->first();
+
+        $this->get(route('contacts.institution', ['subdomain' => 'www', 'lang' => 'lt', 'institution' => $institution->id]))
+            ->assertInertia(fn (Assert $page) => $page
+                ->component('Public/Contacts/ContactInstitutionOrType')
+                ->has('institution')
+                ->has('contacts')
+            );
+    }
+
+    public function test_can_open_representation_padaliniai_category(): void
+    {
+        $this->get(route('contacts.category', ['subdomain' => 'www', 'lang' => 'lt', 'type' => 'padaliniai']))
+            ->assertInertia(fn (Assert $page) => $page
+                ->component('Public/Contacts/ShowContactCategory')
+                ->has('institutions')
             );
     }
 }
