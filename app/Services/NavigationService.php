8372bb7eea090090fb6fa76892361d19c466cbed
Justinas Kavoliūnas <justinaskav@gmail.com>
2024-05-06 20:01:53 +0300

    More comments for the navigation creation in server for UI

--- app/Services/NavigationService.php
+++ app/Services/NavigationService.php
@@ -9,38 +9,52 @@ class NavigationService
 {
     public static function getNavigationForPublic()
     {
+        // Get only navigation for VU SA padalinys
         $vusa = Padalinys::where('shortname', 'VU SA')->first();
 
         $navigation = Navigation::where([['padalinys_id', $vusa->id], ['lang', app()->getLocale()]])->orderBy('order')->get();
 
+        // Start with root navigation elements
         $rootNavigation = $navigation->where('parent_id', 0)->values()->toArray();
 
+        // Build children on root navigation elements
         for ($i = 0; $i < count($rootNavigation); $i++) {
-            // Make array of arrays of links, by columns in extra_attributes
+
+            // The structure that the UI uses is that the root navigation elements have a 'links' property which is an array of max. 3 arrays.
+            // Each array of the links array represents a column in the navigation.
+            // The information about which column the link should be in is stored in the 'extra_attributes->column' property of the link.
             $rootNavigation[$i]['links'] = [];
 
+            // Get immediate children of root navigation element
             $children = $navigation->where('parent_id', $rootNavigation[$i]['id'])->values()->toArray();
 
-            //# Expand extra_attributes to own keys
+            // Expand extra_attributes to own keys to make it easier to work with
+            // Other data in the extra_attributes array will be used in the UI
             foreach ($children as $key => $child) {
                 $extraAttributes = $child['extra_attributes'];
                 unset($child['extra_attributes']);
 
-                // If extra_attributes is null set to empty array
+                // If extra_attributes is null, continue
                 if ($extraAttributes === null) {
-                    $extraAttributes = [];
+                    continue;
                 }
 
+                // Update the child with the extra attributes
                 foreach ($extraAttributes as $extraKey => $extraValue) {
                     $child[$extraKey] = $extraValue;
                 }
 
+                // Update the child by overwriting the old child
                 $children[$key] = $child;
             }
 
+            // Set the links of the root navigation by columns
             for ($j = 1; $j <= 3; $j++) {
+
                 // Push array to root links, where extra_attributes['column'] == $j
                 $rootNavigation[$i]['links'][] = array_filter($children, function ($child) use ($j) {
+
+                    // Also check if the column is not set, then it should be in the first column
                     if (! isset($child['column'])) {
                         return $j == 1;
                     }
@@ -48,14 +62,13 @@ class NavigationService
                     return $child['column'] == $j;
                 });
 
-                // To values
                 $rootNavigation[$i]['links'][$j - 1] = array_values($rootNavigation[$i]['links'][$j - 1]);
             }
 
             // Remove empty arrays
             $rootNavigation[$i]['links'] = array_filter($rootNavigation[$i]['links']);
 
-            // Add column count
+            // Add column count immediately for the front end
             $rootNavigation[$i]['cols'] = count($rootNavigation[$i]['links']);
         }
 
