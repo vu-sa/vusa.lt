cdd015cb2572e18b65944625147b9768c6c51e29
Justinas Kavoliūnas <justinaskav@gmail.com>
2024-07-07 20:20:24 +0300

    Don't remove foreign keys in migrations

--- database/migrations/2024_07_06_003125_remove_saziningai_tables.php
+++ database/migrations/2024_07_06_003125_remove_saziningai_tables.php
@@ -11,21 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // remove foreign keys
-
-        Schema::table('saziningai_observers', function (Blueprint $table) {
-            $table->dropForeign(['exam_uuid']);
-            $table->dropForeign(['flow']);
-        });
-
-        Schema::table('saziningai_exam_flows', function (Blueprint $table) {
-            $table->dropForeign(['exam_uuid']);
-        });
-
-        Schema::table('saziningai_exams', function (Blueprint $table) {
-            $table->dropForeign(['padalinys_id']);
-        });
-
         // drop tables
 
         Schema::dropIfExists('saziningai_observers');
