9b21d21417504e2937619f773f12f3ca5deef839
Justinas Kavoliūnas <justinaskav@gmail.com>
2024-09-12 17:40:40 +0300

    Better error pages

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,3 +3,4 @@
 @section('title', __('Server Error'))
 @section('code', '500')
 @section('message', __('Server Error'))
+@section('extended-message', __('Klaida buvo užfiksuota, einame jos tvarkyti! Jeigu problema išlieka, prašome susisiekti su VU SA IT administratoriumi'))
