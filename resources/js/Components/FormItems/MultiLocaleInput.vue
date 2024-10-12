0a8444d9613f6f0ad687daab28a2688c9eb91881
Justinas Kavoliūnas <justinaskav@gmail.com>
2024-10-12 14:13:13 +0300

    Show translation near MultiLocaleInput

--- resources/js/Components/FormItems/MultiLocaleInput.vue
+++ resources/js/Components/FormItems/MultiLocaleInput.vue
@@ -1,14 +1,30 @@
 <template>
-  <NInput v-if="inputLang === 'lt'" v-model:value="input.lt" :type="inputType" :placeholder="placeholders.lt">
-    <template #suffix>
-      <SimpleLocaleButton v-model:locale="inputLang" />
-    </template>
-  </NInput>
-  <NInput v-else-if="inputLang === 'en'" v-model:value="input.en" :placeholder="placeholders.en" :type="inputType">
-    <template #suffix>
-      <SimpleLocaleButton v-model:locale="inputLang" />
-    </template>
-  </NInput>
+  <div v-if="inputLang === 'lt'" class="w-full">
+    <NInput v-model:value="input.lt" :type="inputType" :placeholder="placeholders.lt">
+      <template #suffix>
+        <SimpleLocaleButton v-model:locale="inputLang" />
+      </template>
+    </NInput>
+    <div class="ml-2 mt-0.5">
+      <button class="text-zinc-400" @click="inputLang = LocaleEnum.EN">
+        🇬🇧<span v-if="input.en" class="ml-1 font-semibold">{{ input.en }}</span>
+        <span v-else class="ml-1 italic">None</span>
+      </button>
+    </div>
+  </div>
+  <div v-else-if="inputLang === 'en'" class="w-full">
+    <NInput v-model:value="input.en" :placeholder="placeholders.en" :type="inputType">
+      <template #suffix>
+        <SimpleLocaleButton v-model:locale="inputLang" />
+      </template>
+    </NInput>
+    <div class="ml-2 mt-0.5">
+      <button class="text-zinc-400" @click="inputLang = LocaleEnum.LT">
+        🇱🇹<span v-if="input.lt" class="ml-1 font-semibold">{{ input.lt }}</span>
+        <span v-else class="ml-1 italic">Nėra</span>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -16,6 +32,7 @@ import { computed, ref } from "vue";
 import { usePage } from "@inertiajs/vue3";
 
 import SimpleLocaleButton from "../Buttons/SimpleLocaleButton.vue";
+import { LocaleEnum } from "@/Types/enums";
 
 const props = defineProps<{
   inputType?: "text" | "textarea";
