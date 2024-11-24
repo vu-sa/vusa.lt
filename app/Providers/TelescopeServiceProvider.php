3a0a030801ba959b586191d334f881794bb1812e
Justinas Kavoliūnas <justinaskav@gmail.com>
2024-11-25 00:47:45 +0200

    Refactor viewTelescope gate

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\EntryType;
 use Laravel\Telescope\IncomingEntry;
@@ -77,10 +78,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     protected function gate()
     {
-        Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                'it@vusa.lt', 'justinas.kavoliunas@vusa.lt',
-            ]);
+        Gate::define('viewTelescope', function (User $user) {
+            return $user->isSuperAdmin();
         });
     }
 }
