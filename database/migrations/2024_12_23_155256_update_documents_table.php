b80a6fc92744005e75caaf76376a23bbadf0bae8
Justinas Kavoliūnas <justinaskav@gmail.com>
2024-12-23 15:54:59 +0200

    Longer documents name and title

---
+++ database/migrations/2024_12_23_155256_update_documents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->string('name', 500)->change();
+            $table->string('title', 500)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->string('name', 200)->change();
+            $table->string('title', 200)->change();
+        });
+    }
+};
