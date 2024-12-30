408a4895adb8f833264d73762e02a034a9d16099
justinaskav <justinaskav@users.noreply.github.com>
2024-12-30 12:58:57 +0000

    PHP Linting (Pint)

--- tests/Feature/Other/OtherTest.php
+++ tests/Feature/Other/OtherTest.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Models\Permission;
-use Database\Seeders\ModelPermissionSeeder;
-
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
 test('user can set own settings')->todo();
