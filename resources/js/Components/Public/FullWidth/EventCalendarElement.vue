7c2b8c18776e53bc976a37debf89b4ccc6d0c0e1
Justinas Kavoliūnas <justinaskav@gmail.com>
2025-02-15 19:35:16 +0200

    Remove Calendar Cards from front page

--- resources/js/Components/Public/FullWidth/EventCalendarElement.vue
+++ resources/js/Components/Public/FullWidth/EventCalendarElement.vue
@@ -1,5 +1,5 @@
 <template>
-<Head>
+  <Head>
     <link rel="preload" href="/images/photos/pirmakursiu_stovykla_kaune.jpg" as="image">
     <link rel="preload" href="/images/photos/vu.jpg" as="image">
     <link rel="preload" href="/images/photos/stovykla.jpg" as="image">
@@ -53,36 +53,6 @@
       </div>
     </div>
   </div>
-  <div v-if="upcomingEvents.length > 0" class="my-8">
-    <h2 class="mb-4 text-center lg:text-start">
-      {{ $t('Artėjantys renginiai') }}
-    </h2>
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
-      <a v-for="event in eventsByHavingImages.hasImages" :key="event.id" class="hidden w-full md:block"
-        :href="getCalendarEvent2Route(event, $page.props.app.locale)">
-        <CalendarCard hide-footer :calendar-event="event" />
-      </a>
-      <div v-for="event in eventsByHavingImages.noImages" :key="event[0].id" class="hidden h-full md:block">
-        <div class="grid grid-rows-2 items-start gap-4">
-          <a v-for="subEvent in event" :key="subEvent.id" :href="route('calendar.event', {
-            calendar: subEvent.id,
-            lang: $page.props.app.locale,
-          })
-            ">
-            <CalendarCard hide-footer :calendar-event="subEvent" />
-          </a>
-        </div>
-      </div>
-      <div class="block w-fit md:hidden">
-        <div class="flex flex-col gap-4">
-          <a v-for="event in upcomingEvents.slice(0, 3)" :key="event.id"
-            :href="getCalendarEvent2Route(event, $page.props.app.lang)">
-            <CalendarCard :calendar-event="event" />
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -90,39 +60,14 @@ import { Head } from "@inertiajs/vue3";
 import { NButton } from "naive-ui";
 import { computed, ref } from "vue";
 
-import CalendarCard from "@/Components/Calendar/CalendarCard.vue";
 import CalendarSyncModal from "@/Components/Modals/CalendarSyncModal.vue";
 import EventCalendar from "@/Components/Calendar/EventCalendar.vue";
 import FadeTransition from "@/Components/Transitions/FadeTransition.vue";
-import { getCalendarEvent2Route } from "@/Utils/Route";
 
 const props = defineProps<{
   calendar: Array<App.Entities.Calendar>;
   showPhotos: boolean;
-  upcomingEvents: Array<App.Entities.Calendar>;
 }>();
 
 const showModal = ref(false);
-
-const eventsByHavingImages = computed(() => {
-  const data = {
-    hasImages: props.upcomingEvents.filter((event) => event?.images.length > 0),
-    // Generate array of arrays of two elements, because images take more space
-    noImages: props.upcomingEvents.filter((event) => event?.images.length === 0).reduce((acc, event, index) => {
-      if (index % 2 === 0) {
-        acc.push([event]);
-      } else {
-        acc[acc.length - 1].push(event);
-      }
-      return acc;
-    }, []),
-  };
-
-  // if data noImages last array has one element, remove array
-  if (data.noImages.length > 0 && data.noImages[data.noImages.length - 1].length === 1) {
-    data.noImages.pop();
-  }
-
-  return data;
-});
 </script>
