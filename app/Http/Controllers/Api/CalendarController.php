1b7f81db2d38ece75f80db145867d4e94869eb51
justinaskav <justinaskav@users.noreply.github.com>
2025-02-16 23:39:34 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/Api/CalendarController.php
+++ app/Http/Controllers/Api/CalendarController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Calendar;
-use App\Models\Tenant;
 use Illuminate\Support\Facades\Cache;
 
 class CalendarController extends Controller
