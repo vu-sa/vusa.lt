90076077d2ab19cdd24388c9ecfdbb2fe49e4fb4
Justinas Kavoliūnas <justinaskav@gmail.com>
2025-03-04 19:18:07 +0200

    Update NewsElement

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -16,8 +16,8 @@ class NewsController extends Controller
         $news = News::query()->where([['tenant_id', '=', $tenant->id], ['lang', $lang], ['draft', '=', 0]])
             ->where('publish_time', '<=', date('Y-m-d H:i:s'))
             ->orderBy('publish_time', 'desc')
-            ->take(4)
-            ->get(['id', 'title', 'lang', 'publish_time', 'permalink', 'image']);
+            ->take(5)
+            ->get(['id', 'title', 'lang', 'short', 'publish_time', 'permalink', 'image']);
 
         return response()->json($news);
     }
--- resources/js/Components/Public/NewsElement.vue
+++ resources/js/Components/Public/NewsElement.vue
@@ -1,39 +1,89 @@
 <template>
-  <div v-if="news.length > 0" class="my-8 rounded-lg py-4">
-    <header class="mb-4 flex flex-wrap items-center justify-between gap-1">
-      <div>
-        <h2 class="lg:mb-0 text-4xl">
-          {{ $t(element.json_content?.title) }}
-        </h2>
-        <!-- <p class="text-zinc-800 dark:text-zinc-50">
-          {{
-            $t("Karščiausios universiteto naujienos tiesiai iš studentų lūpų")
-          }}!
-        </p> -->
-      </div>
-      <SmartLink :href="route('newsArchive', {
-        subdomain: $page.props.tenant?.subdomain ?? 'www',
-        lang: $page.props.app.locale === 'lt' ? 'lt' : 'en',
-        newsString: $page.props.app.locale === 'lt' ? 'naujienos' : 'news',
-      })
-        ">
-        <div class="inline-flex items-center gap-1">
-          <span>{{ $t("Daugiau") }}</span>
-          <IFluentArrowRight16Regular />
-        </div>
-      </SmartLink>
-    </header>
+  <div v-if="news.length > 0" class="my-4 rounded-lg py-4">
+    <!-- <header class="mb-4 flex flex-wrap items-center justify-between gap-1"> -->
+    <!--   <div> -->
+    <!--     <h2 class="lg:mb-0 text-4xl"> -->
+    <!--       {{ $t(element.json_content?.title) }} -->
+    <!--     </h2> -->
+    <!--   </div> -->
+    <!-- </header> -->
 
-    <div class="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-      <SmartLink v-for="item in news" :key="item.id" prefetch :href="route('news', {
-        lang: item.lang,
-        news: item.permalink ?? '',
-        newsString: 'naujiena',
-        subdomain: $page.props.tenant?.subdomain ?? 'www',
-      })">
-        <NewsCard :news="item" />
-      </SmartLink>
-    </div>
+    <section class="grid gap-8 sm:grid-cols-1 md:grid-cols-[2fr_1fr]">
+      <!-- <SmartLink v-for="item in news" :key="item.id" prefetch :href="route('news', { -->
+      <!--   lang: item.lang, -->
+      <!--   news: item.permalink ?? '', -->
+      <!--   newsString: 'naujiena', -->
+      <!--   subdomain: $page.props.tenant?.subdomain ?? 'www', -->
+      <!-- })"> -->
+      <!--   <NewsCard :news="item" /> -->
+      <!-- </SmartLink> -->
+      <div v-if="firstNews">
+        <SmartLink :href="route('news', {
+          lang: firstNews.lang,
+          news: firstNews.permalink ?? '',
+          newsString: 'naujiena',
+          subdomain: $page.props.tenant?.subdomain ?? 'www',
+        })">
+          <img :src="firstNews.image" alt="News image"
+            class="mb-1 max-h-84 w-full rounded-xs object-cover shadow-md duration-200 hover:shadow-lg">
+        </SmartLink>
+        <p class="text-zinc-500">
+          {{ formatStaticTime(new Date(firstNews.publish_time), { year: "numeric", month: "long", day: "numeric" },
+            $page.props.app.locale) }}
+        </p>
+        <SmartLink :href="route('news', {
+          lang: firstNews.lang,
+          news: firstNews.permalink ?? '',
+          newsString: 'naujiena',
+          subdomain: $page.props.tenant?.subdomain ?? 'www',
+        })">
+          <p
+            class="mt-3 w-fit font-extrabold text-3xl leading-tighter text-zinc-800 line-clamp-2 dark:text-zinc-50 hover:text-vusa-red">
+            {{ firstNews.title }}
+          </p>
+        </SmartLink>
+        <div class="leading-tight mt-3 text-zinc-700">
+          <div v-html="firstNews.short" />
+        </div>
+      </div>
+      <div class="flex flex-col gap-4">
+        <p class="text-2xl font-bold">
+          {{ $t("Naujausios") }}
+        </p>
+        <div v-for="item in otherNews" :key="item.id" class="grid grid-cols-[1fr_2fr] gap-4 items-center">
+          <SmartLink :href="route('news', {
+            lang: item.lang,
+            news: item.permalink ?? '',
+            newsString: 'naujiena',
+            subdomain: $page.props.tenant?.subdomain ?? 'www',
+          })">
+            <img :src="item.image" alt="News image"
+              class="mb-1 max-h-32 md:max-h-40 w-full rounded-xs object-cover shadow-md duration-200 hover:shadow-lg">
+          </SmartLink>
+          <SmartLink :href="route('news', {
+            lang: item.lang,
+            news: item.permalink ?? '',
+            newsString: 'naujiena',
+            subdomain: $page.props.tenant?.subdomain ?? 'www',
+          })">
+            <span class="text-zinc-800 hover:text-vusa-red font-semibold leading-tight lg:text-base line-clamp-3">
+              {{ item.title }}
+            </span>
+          </SmartLink>
+        </div>
+        <SmartLink :href="route('newsArchive', {
+          subdomain: $page.props.tenant?.subdomain ?? 'www',
+          lang: $page.props.app.locale === 'lt' ? 'lt' : 'en',
+          newsString: $page.props.app.locale === 'lt' ? 'naujienos' : 'news',
+        })
+          ">
+          <div class="inline-flex items-center gap-1 font-bold">
+            <span>{{ $t("Žiūrėti visas") }}</span>
+            <IFluentArrowRight16Regular />
+          </div>
+        </SmartLink>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -44,9 +94,11 @@ import { usePage } from "@inertiajs/vue3";
 import SmartLink from "./SmartLink.vue";
 import NewsCard from "../Cards/NewsCard.vue";
 import type { News } from '@/Types/contentParts';
+import { computed } from "vue";
+import { formatStaticTime } from "@/Utils/IntlTime";
 
 defineProps<{
-  element: News;  
+  element: News;
 }>();
 
 const news = await fetch(
@@ -54,5 +106,9 @@ const news = await fetch(
     lang: usePage().props.app.locale,
     tenant: usePage().props.tenant?.alias,
   })
-).then((response) => response.json());
+).then((response) => response.json()) as App.Entities.News[];
+
+const firstNews = computed(() => news[0]);
+
+const otherNews = computed(() => news.slice(1));
 </script>
