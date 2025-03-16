8e6b2f23254f2d4793342534f757cd5fb7508565
Justinas Kavoliūnas <justinaskav@gmail.com>
2025-03-16 13:41:16 +0200

    Add Storybook a11y

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,9 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-coverage",
+    "@storybook/addon-a11y"
   ],
   "framework": {
     "name": "@storybook/vue3-vite",
