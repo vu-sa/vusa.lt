9500dcd3aea97b344f24a458c545185aec9e62ed
Justinas Kavoliūnas <justinaskav@gmail.com>
2025-04-11 01:11:32 +0300

    Fix frontend testing

--- tests/Unit/useAdminTable.spec.ts
+++ tests/Unit/useAdminTable.spec.ts
@@ -153,3 +153,9 @@ describe('useAdminTable composable', () => {
     expect(reloadSpy).toHaveBeenCalled();
   });
 });
+
+describe('Simple Test', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
