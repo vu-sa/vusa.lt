375d4ada1ffef8ef4433fcdb6e8c78b1087adc8b
Justinas Kavoliūnas <justinaskav@gmail.com>
2025-12-11 00:31:51 +0200

    Add middleware to block search engine indexing on staging domains

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array<int, string>
      */
     protected $middleware = [
+        \App\Http\Middleware\BlockRobotsOnStagingDomains::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
