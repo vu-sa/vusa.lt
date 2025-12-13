37b27cd329cb45dcfc6afaf5b21f0905cf223d01
Justinas Kavoliūnas <justinaskav@gmail.com>
2025-12-13 15:05:35 +0200

    Add vendor as a bypassable segment

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -55,7 +55,7 @@ class SetLocale
 
     protected function shouldBypassLocale($segment)
     {
-        $bypassSegments = ['mano', 'auth', 'feedback', 'login', 'telescope', '_impersonate', 'feed', 'livewire', 'registration'];
+        $bypassSegments = ['mano', 'auth', 'feedback', 'login', 'telescope', '_impersonate', 'feed', 'livewire', 'registration', 'vendor'];
 
         // Bypass sitemap routes
         if (is_string($segment) && (str_starts_with($segment, 'sitemap') || str_ends_with($segment, '.xml'))) {
