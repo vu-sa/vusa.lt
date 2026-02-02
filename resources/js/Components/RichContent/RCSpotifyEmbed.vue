78d03576e152e31e29c06450eef1007aecf37636
Justinas Kavoliūnas <justinaskav@gmail.com>
2026-02-03 00:36:37 +0200

    Theme support for spotify element

--- resources/js/Components/RichContent/RCSpotifyEmbed.vue
+++ resources/js/Components/RichContent/RCSpotifyEmbed.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="w-full my-8">
-    <iframe class="rounded-xl" :src="element.json_content.url" width="100%" height="352" frameborder="0" allowtransparency="true"
+    <iframe class="block w-full h-[352px] rounded-xl" :src="embedUrl" frameborder="0" allowtransparency="true"
       allow="encrypted-media" title="Spotify Embed" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+import { useDark } from "@vueuse/core";
 import type { SpotifyEmbed } from '@/Types/contentParts';
 
-defineProps<{
+const props = defineProps<{
   element: SpotifyEmbed;
 }>();
+
+const isDark = useDark();
+
+const embedUrl = computed(() => {
+  const url = props.element.json_content.url;
+  try {
+    const parsedUrl = new URL(url, window.location.origin);
+    parsedUrl.searchParams.set('theme', isDark.value ? '0' : '1');
+    return parsedUrl.toString();
+  } catch {
+    const themeParam = `theme=${isDark.value ? '0' : '1'}`;
+    return url.includes('?') ? `${url}&${themeParam}` : `${url}?${themeParam}`;
+  }
+});
 </script>
