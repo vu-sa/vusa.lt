2ecdb3a32926103c154d801ed4820ab6bb52ba59
Justinas Kavoliūnas <justinaskav@gmail.com>
2026-05-31 15:10:00 +0300

    Update deps

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 13.11.2.
+ * Generated for Laravel 13.12.0.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
